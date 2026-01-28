fc3ed0afe9cfb828866b734c55b541411cf0e3f7
Omremotebricks <omremotebricks@gmail.com>
2026-01-28 15:24:40 +0530

    Refactor BackgroundMap component to use imported map asset and clean up inline styles

--- src/components/BackgroundMap.tsx
+++ src/components/BackgroundMap.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React from "react";
+import map from "../assets/world-map-transparent.png";
 export const BackgroundMap: React.FC = () => {
   return (
     <div className="w-full h-full relative overflow-hidden">
@@ -10,25 +10,25 @@ export const BackgroundMap: React.FC = () => {
       */}
       <div className="flex w-[200%] h-full animate-scroll will-change-transform">
         {/* First copy of the map */}
-        <div 
+        <div
           className="w-1/2 h-full bg-gray-200"
-          style={{ 
-            backgroundImage: "url('/world-map-transparent.png')",
-            backgroundSize: '120%',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat',
-            opacity: 0.4
+          style={{
+            backgroundImage: `url(${map})`,
+            backgroundSize: "120%",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            opacity: 0.4,
           }}
         ></div>
         {/* Second copy of the map for seamless looping */}
-        <div 
+        <div
           className="w-1/2 h-full bg-gray-200"
-          style={{ 
-            backgroundImage: "url('/world-map-transparent.png')",
-            backgroundSize: '120%',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat',
-            opacity: 0.4
+          style={{
+            backgroundImage: `url(${map})`,
+            backgroundSize: "120%",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            opacity: 0.4,
           }}
         ></div>
       </div>
